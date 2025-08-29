7c7746ac857665949f7ed29964ca926c70b8400e
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-08-29 15:38:59 +0300

    ex3 is completed

--- server.js
+++ server.js
@@ -15,13 +15,30 @@ app.get('/roll/:number', (req, res) => {
 
     if (isNaN(rollNumber)) {
         return res.send(`<h1>You must specify a number.</h1>`);
-    } else if (rollNumber >= 0 && rollNumber <= 9) {
-        return (rollNumber - 2);
     };
 
-    res.send(`<h1>You rolled a ${rollNumber - 2}.</h1>`);
+    res.send(`<h1>Your Majesty, you've rolled a ${rollNumber - 2}.</h1>`);
 
 });
+
+app.get('/collectibles/:index', (req, res) => {
+    const collectibles = [
+        { name: 'shiny ball', price: 5.95 },
+        { name: 'autographed picture of a dog', price: 10 },
+        { name: 'vintage 1970s yogurt SOLD AS-IS', price: 0.99 }
+    ];
+
+    const collectible = collectibles[req.params.index];
+
+    if (collectible) {
+        res.send(`<h1>Hey customer, the price for ${collectible.name} is $${collectible.price}.</h2>`);
+    } else {
+        return res.send(`<h1>Unfortunately the item is not available, we will contact you soon when the item is instock.</h1>`);
+    };
+
+});
+
+
 
 app.listen(3000, () => {
     console.log('Listening on app 3000');
