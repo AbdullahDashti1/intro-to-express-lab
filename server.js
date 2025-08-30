b365a98512f5d12af081f60c932ef2a8efc2d68a
Abdulla Dashti <abdullamustafadashti.0@gmail.com>
2025-08-30 10:35:22 +0300

    ex2 adjusted ex4 not completed

--- server.js
+++ server.js
@@ -17,7 +17,9 @@ app.get('/roll/:number', (req, res) => {
         return res.send(`<h1>You must specify a number.</h1>`);
     };
 
-    res.send(`<h1>Your Majesty, you've rolled a ${rollNumber - 2}.</h1>`);
+    const rolledNumber = Math.floor(Math.random() * rollNumber) + 0
+
+    res.send(`<h1>Your Majesty, you've rolled a ${rolledNumber}.</h1>`);
 
 });
 
@@ -38,7 +40,43 @@ app.get('/collectibles/:index', (req, res) => {
 
 });
 
+app.get('/shoes', (req, res) => {
+    const shoes = [
+        { name: "Birkenstocks", price: 50, type: "sandal" },
+        { name: "Air Jordans", price: 500, type: "sneaker" },
+        { name: "Air Mahomeses", price: 501, type: "sneaker" },
+        { name: "Utility Boots", price: 20, type: "boot" },
+        { name: "Velcro Sandals", price: 15, type: "sandal" },
+        { name: "Jet Boots", price: 1000, type: "boot" },
+        { name: "Fifty-Inch Heels", price: 175, type: "heel" }
+    ];
 
+    const minPrice = req.query["min-price"];
+    const maxPrice = req.query["max-price"];
+    const type = req.query.type;
+
+    let result = [];
+
+    for (let shoe of shoes) {
+        let isValid = true;
+
+        if (minPrice && shoe.price < Number(minPrice)) {
+            isValid = false;
+        }
+        if (maxPrice && shoe.price > Number(maxPrice)) {
+            isValid = false;
+        }
+        if (type && shoe.type !== type) {
+            isValid = false;
+        }
+
+        if (isValid) {
+            result.push(shoe);
+        }
+    }
+
+    res.send(result);
+});
 
 app.listen(3000, () => {
     console.log('Listening on app 3000');
